cff692aaf4bac4694442c59e7c3332d2b1e9e7cb
PawiXer <u22epawab@liceum.p.lodz.pl>
2023-11-21 11:21:16 +0100

    kochamProgramowanie

---
+++ main.py
@@ -0,0 +1,18 @@
+def celsius_to_kelvin(temperature):
+    return temperature + 273.15
+
+
+if __name__ == '__main__':
+    with open("input.txt", "r") as file, open("input.txt", "r") as result:
+        lines = file.readlines()
+        print(lines)
+        temperatures = []
+        for line in lines:
+            temperature = float(line.strip())
+            temperatures.append(temperature)
+        print(lines)
+        print(temperatures)
+        result.write("Celsius; Kelvin\n")
+        for temperature in temperatures:
+            result.write(f"{temperature}; {celsius_to_kelvin(temperature)}\n")
+
